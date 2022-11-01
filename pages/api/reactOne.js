001828cfd024a52c54550e5ea6ab48ace3f8ecd4
hasindu sithmin <hasindusithmin64@gmail.com>
2022-11-01 07:34:27 +0000

    Update index.js

---
+++ pages/api/reactOne.js
@@ -0,0 +1,9 @@
+// import pool from "../../db";
+// export default async function handler(req,res){
+//     try {
+//         const {id} = req.query;
+//         if (id === undefined) th
+//     } catch (error) {
+        
+//     }
+// }
--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,17 @@ import Head from "next/head"
 export default function Home() {
 
   const [stock, setStock] = useState(null)
-
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     fetch('/api/read')
-      .then(res => {
-        if (res.status === 200) return res.json()
-        // else throw res.json()
-      })
-      .then(data => {
-        setStock(data)
+      .then(async (res) => {
+        const data = await res.json()
+        if (res.status === 200) setStock(data)
+        else throw Error(data['error'])
       })
       .catch(error => {
-        console.log(error.message);
+        setError(error.message)
       })
   }, [])
 
@@ -38,37 +36,45 @@ export default function Home() {
         <p><b>Stock Dashboard</b></p>
       </header>
       <div className="w3-responsive">
-        <table className="w3-table-all">
-          <thead>
-            <tr className="w3-dark-gray">
-              <th>Date</th>
-              <th>Vendor</th>
-              <th>Item</th>
-              <th>Quantity</th>
-              <th>Unit Price</th>
-              <th>Total Cost</th>
-              <th className="w3-center">Update</th>
-              <th className="w3-center">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              stock &&
-              stock.map(({ id,date, vendor, item, quantity, unit_price, total_cost }) => 
-                <tr key={id} id={id}>
-                  <td>{date}</td>
-                  <td>{vendor}</td>
-                  <td>{item}</td>
-                  <td>{quantity}</td>
-                  <td>{unit_price}</td>
-                  <td>{total_cost}</td>
-                  <td className="w3-center" onClick={() => { updRow(id) }}><AiFillEdit /></td>
-                  <td className="w3-center" onClick={() => { delROw(id) }}><RiDeleteBin2Fill /></td>
-                </tr>
-              )
-            }
-          </tbody>
-        </table>
+        {
+          error &&
+          <div className="w3-panel w3-leftbar w3-pale-red w3-large w3-serif">
+            <p><i>'{error}.'</i></p>
+          </div>
+        }
+        {
+          stock &&
+          <table className="w3-table-all">
+            <thead>
+              <tr className="w3-dark-gray">
+                <th>Date</th>
+                <th>Vendor</th>
+                <th>Item</th>
+                <th>Quantity</th>
+                <th>Unit Price</th>
+                <th>Total Cost</th>
+                <th className="w3-center">Update</th>
+                <th className="w3-center">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                stock.map(({ id, date, vendor, item, quantity, unit_price, total_cost }) =>
+                  <tr key={id} id={id}>
+                    <td>{date}</td>
+                    <td>{vendor}</td>
+                    <td>{item}</td>
+                    <td>{quantity}</td>
+                    <td>{unit_price}</td>
+                    <td>{total_cost}</td>
+                    <td className="w3-center" onClick={() => { updRow(id) }}><AiFillEdit /></td>
+                    <td className="w3-center" onClick={() => { delROw(id) }}><RiDeleteBin2Fill /></td>
+                  </tr>
+                )
+              }
+            </tbody>
+          </table>
+        }
       </div>
       <div className="w3-padding"></div>
     </>
