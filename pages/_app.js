2e900f57b20fc2b2e549db3bd80747b3983ee89a
hasindusithmin <hasindusithmin64@gmail.com>
2022-11-01 11:34:15 +0530

    Updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import '../styles/w3.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <div className='w3-content' style={{maxWidth:'1500px'}} >
+        <Component {...pageProps} />
+      </div>
+  )
 }
 
 export default MyApp
